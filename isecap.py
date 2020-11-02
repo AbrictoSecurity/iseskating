ae0bb669c3cd06d800b638f6b2d7dabdcee81dc0
root <root@ise-skate.attlocal.net>
2020-11-02 13:55:37 +0000

    adding services

--- isecap.py
+++ isecap.py
@@ -10,7 +10,7 @@ pcapf = "/usr/share/iseskate/PCAP/"
 
 # Conducting a continuous packet capture
 def data_capture():
-    pass_cap = "tcpdump port http or port ftp or port smtp or port imap or port pop3 or port telnet -lA | egrep -i " \
+    pass_cap = "tcpdump -i eth0 port http or port ftp or port smtp or port imap or port pop3 or port telnet -lA | egrep -i " \
                "-B5 'pass=|pwd=|log=|login=|user=|username=|pw=|passw=|passwd= " \
                "|password=|pass:|user:|username:|password:|login:|pass |user ' > " + pcapf + "clear_text_passwords.txt"
 
